40394e14db356a60b261cb7fa883b3aa3976a381
Qiqi Xiao <xiaoqiqi1771@126.com>
2018-03-22 08:52:51 -0400

    modify bblist feature extracting

--- extractor/feature_extractor_bbslist.py
+++ extractor/feature_extractor_bbslist.py
@@ -27,18 +27,19 @@ if __name__ == '__main__':
 
     model_file = 'faster_rcnn_pytorch/models/VGGnet_fast_rcnn_iter_70000.h5'
     extractor = build_extractor(model_file)
+    
     #features_dets = []
-    MAX_NO = 250
     
     pkl_dir = '../pkls/vot_features_{}_bbslist_{}'.format(dataset_name, classname)
     if not os.path.exists(pkl_dir):
         os.mkdir(pkl_dir)
     for img_path, bbs in zip(img_paths, bbslist):
-        dets = np.array([[0.] + list(bb[:-1]) for bb in bbs[:MAX_NO]], dtype='float32')
-        DEBUG = True
+        img_path = os.path.join('../', img_path.split('/', 6)[-1])
+        dets = np.array([[0., bb[1], bb[0], bb[3], bb[2]] for bb in bbs], dtype='float32')
+        DEBUG = False
         if DEBUG:
             img = cv2.imread(img_path)
-            for det in dets:
+            for det in dets[:30]:
                 cv2.rectangle(img, (int(det[1]), int(det[2])), (int(det[3]), int(det[4])), (0, 204, 0), 2)
             cv2.imshow('img', img)
             cv2.waitKey(0)
@@ -46,5 +47,5 @@ if __name__ == '__main__':
         feature_dets = feature_dets.data.cpu().numpy()
         #features_dets.append(feature_dets)
         img_id = img_path.split('/')[-1].split('.')[0]
-        dets_pkl = os.path.join(pkl_dir, 'vot_features_{}_bbslist_{}_{}.pkl'.format(dataset_name, classname, img_id)
+        dets_pkl = os.path.join(pkl_dir, 'vot_features_{}_bbslist_{}_{}.pkl'.format(dataset_name, classname, img_id))
         pkl.dump(feature_dets, open(dets_pkl, 'wb'))
