ada22a3bd2f82c12edc048a322dc1ac2fb419999
Qiqi Xiao <xiaoqiqi1771@126.com>
2018-04-06 16:56:06 -0400

    add sh2

--- extractor/faster_rcnn_pytorch/train_optha_part.sh
+++ extractor/faster_rcnn_pytorch/train_optha_part.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=$1 python train_optha_part.py --datasetname='trainval' --ratio-name='424'
+CUDA_VISIBLE_DEVICES=$1 python train_optha_part.py --datasetname='trainval' --ratio-name='226'
