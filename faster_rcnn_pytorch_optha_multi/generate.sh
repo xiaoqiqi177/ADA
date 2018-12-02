640fb2ab4b3de8b69c462f47981ea90df3654972
Qiqi Xiao <xiaoqiqi1771@126.com>
2018-12-02 10:39:15 -0500

    rm trash

--- faster_rcnn_pytorch_optha_multi/generate.sh
+++
@@ -1,6 +0,0 @@
-DOUBLE='double_full_healthy'
-TIME=2
-
-python generate.py --task='ma_'$DOUBLE --ifhealthy --ifimage --ifhealthy --ratio-name='721' --size-times=$TIME
-#python generate.py --task='ma_'$DOUBLE --ifimage --ratio-name='721' --size-times=$TIME
-
--- optha_classification/generate_bbox_augmentation.sh
+++
@@ -1,2 +0,0 @@
-python generate_bbox_augmentation.py --task='ma' --ispart  --ifimage --ratio-name='721'
-#python generate_bbox.py --task='ma' --ispart --ifskip --ifimage --ratio-name='721' --ifdebug
